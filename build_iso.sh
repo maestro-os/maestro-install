a6b68bf5db5273cd46a65b6bd781a0b1b555421f
llenotre <llenotre@student.42.fr>
2022-11-14 22:06:16 +0100

    Added ISO creation

--- build_iso.sh
+++ build_iso.sh
@@ -1,8 +1,23 @@
 #!/bin/sh
 
+# Installer compilation
 cargo build --release
 
-mkdir -pv mnt/sbin
-cp target/release/maestro_install mnt/sbin/init
 
-find mnt/ | cpio -o >initramfs
+
+# Preparing grub
+mkdir -pv iso/boot/grub/
+cp -v grub.cfg iso/boot/grub/
+# TODO Install kernel
+
+
+
+# Initramfs creation
+mkdir -pv mnt/sbin
+cp -v target/release/maestro_install mnt/sbin/init
+find mnt/ | cpio -o >iso/boot/initramfs
+
+
+
+# Grub setup
+grub-mkrescue -o maestro.iso iso
